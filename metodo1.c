06d0e423f3caaddff9f388b0f5801c579e364246
WilliamArantes <williamarantes@id.uff.br>
2017-09-04 21:34:28 -0300

    Lendo a Matriz

--- metodo1.c
+++ metodo1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(){
+	
+	int i,j,k,n = 4;
+	float M[n][n+1],c,soma = 0.0,V[n];
+
+//Entrando com os valores na matriz
+
+	for(i=1;i<=n;i++){
+		printf("Elementos da %da. linha:\n",(i));
+		for(j=1;j<=n+1;j++){
+
+			printf("M[%d][%d] = ",i,j);
+			scanf("%f", &M[i][j]);
+			}
+
+			printf("\n");
+			}
